a8b5bafc32038a36b573d03897177c812306d52a
William Bainton <baintonw@gmail.com>
2019-05-06 22:26:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,10 +17,7 @@ def first_challenge
     }
   }
   
-contacts.each do |person, data|
-
-
-  data.each do |attribute, value|
+contacts["Freddy Mercury"].each do |attribute, value|
 
     if attribute == :favorite_icecream_flavors
       binding.pry
