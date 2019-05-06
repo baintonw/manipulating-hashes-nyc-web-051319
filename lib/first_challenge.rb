3bec9689c38b8d7d48d7df13b1a7323af44d8e93
William Bainton <baintonw@gmail.com>
2019-05-06 21:56:02 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -30,7 +30,7 @@ contacts.each do |person, data|
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
         if flavor == "strawberry"
-          delete flavor
+          flavor.delete
         end
       end
     end
